5b87a53b7576cf3d96aa5a30ebcca1bf4605b7aa
Zachary Donato <zdonato@stevens.edu>
2017-03-14 12:19:38 -0400

    Adds mergeid to json. Fixes error for last action not getting next or error actions.

--- convert.js
+++ convert.js
@@ -26,7 +26,7 @@ const eventAction = {
  * @param headerRow : Number - The row corresponding to the header of the sheet, defaults to 0
  */
 function flat(filename, output, isSeries, headerRow = 0) {
-    let actions = buildActions(filename, headerRow);
+    let actions = buildActions(filename, headerRow, 'flat');
 
     if (isSeries) {
         actions = series(actions);
@@ -44,7 +44,7 @@ function flat(filename, output, isSeries, headerRow = 0) {
  * @param headerRow : Number - The number pertaining to the header row of the input file
  */
 function workflow(filename, output, isSeries, headerRow = 0) {
-    let actions = buildActions(filename, headerRow),
+    let actions = buildActions(filename, headerRow, 'workflow'),
         baseObj = actions.splice(0,1)[0];
 
     addNestedActions(baseObj, actions);
@@ -80,12 +80,13 @@ function series(actions) {
  * Method to build an array of actions from an excel spreadsheet.
  *
  * @param filename : String - The name of the excel spreadsheet.
- * @param headerRow : Number - The row corresponding to the header of the sheet
+ * @param headerRow : Number - The row corresponding to the header of the
+ * @param type : String - "workflow" or "flat" to determine mergeid;
  *
  * @return Returns an array of actions
  */
 
-function buildActions(filename, headerRow) {
+function buildActions(filename, headerRow, type) {
     let file = xlsx.readFile(filename),
         actions = [],
         sheetNames = file.SheetNames;
@@ -133,6 +134,8 @@ function buildActions(filename, headerRow) {
                     }
                 };
 
+                action.options.mergeid = (type === 'workflow') ? "update_grid_error_actions" : "update_grid_actions";
+
                 let A = "A" + i,
                     B = "B" + i,
                     C = "C" + i,
@@ -176,18 +179,20 @@ function buildActions(filename, headerRow) {
  * @param actions: Array - The array of actions to add
  */
  function addNestedActions(obj, actions) {
-     if (actions.length === 0) {
-         return;
-     }
-
      obj.options = obj.options || {};
      // Add the keyMap.
      obj.options.keyMap = {
          resultsKey: "data"
      };
      obj.options.nextActions = [];
+     obj.options.errorActions = [eventAction];
      // Add the event Action.
      obj.options.nextActions.push(eventAction);
+
+     if (actions.length === 0) {
+         return;
+     }
+
      // Add the ajax action.
      obj.options.nextActions.push(actions.splice(0,1)[0]);
 
